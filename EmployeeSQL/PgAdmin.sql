9ce585f784ff86005401ba908c725e20f493f358
Nelson Ngwenya <bsimms0909@yahoo.com>
2023-11-06 23:43:46 -0600

    Updated Module 9

--- EmployeeSQL/PgAdmin.sql
+++ EmployeeSQL/PgAdmin.sql
@@ -1,17 +1,18 @@
---List the employee number, last name, first name, sex, and salary of each employ
--- aliases E as Employees and S as Salaries
+--Data Analysis
+
+--List the employee number, last name, first name, sex, and salary of each employee.
+-- aliases Employees as E, Salaries as S
 SELECT E.emp_no, E.last_name, E.first_name, E.sex, S.salary
 FROM Employees AS E
 INNER JOIN Salaries AS S ON E.emp_no = S.emp_no;
 
---List the first name, last name, and hire date for the employees who were hired in 1986
+--List the first name, last name, and hire date for the employees who were hired in 1986.
 SELECT first_name, last_name, hire_date
 FROM Employees
 WHERE EXTRACT(YEAR FROM hire_date) = 1986;
 
-
 --List the manager of each department along with their department number, department name, employee number, last name, and first name.
---aliases :Department_Mgr as DM,Departments as D, Employees as E.
+--aliases :Department_Mgr as DM, Departments as D, Employees as E.
 SELECT
     DM.emp_no AS manager_emp_no,
     D.dept_no,
@@ -51,13 +52,22 @@ WHERE DE.dept_no = 'd007';
 --List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
 --aliases :Employees as E, Department_Emp as DE, Departments as D.
 SELECT E.emp_no, E.last_name, E.first_name, D.dept_name
-FROM Employees E
-JOIN Department_Emp DE ON E.emp_no = DE.emp_no
-JOIN Departments D ON DE.dept_no = D.dept_no
-WHERE D.dept_no IN ('d007', 'd005');
+FROM Employees AS E
+JOIN Departments AS D ON E.dept_id = D.dept_id
+WHERE D.dept_name IN ('Sales', 'Development');
+
+--List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT E.emp_no, E.last_name, E.first_name, D.dept_name
+FROM Employees AS E
+JOIN Departments AS D ON E.dept_id = D.dept_id
+WHERE D.dept_name IN ('Sales', 'Development');
+
 
 --List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
 SELECT last_name, COUNT(*) as name_count
 FROM Employees
 GROUP BY last_name
 ORDER BY name_count DESC, last_name;
+
+
+----need to look at lines 52-63
--- EmployeeSQL/README.md
+++ EmployeeSQL/README.md
@@ -1,4 +1,5 @@
 # sql-challenge
+
 Entity Relationship Diagram of the tables
 # The Departments:
 Departments
@@ -28,7 +29,6 @@ SELECT first_name, last_name, hire_date
 FROM Employees
 WHERE EXTRACT(YEAR FROM hire_date) = 1986;
 
-
 List the manager of each department along with their department number, department name, employee number, last name, and first name.
 --List the manager of each department along with their department number, department name, employee number, last name, and first name.
 --aliases :Department_Mgr as DM,Departments as D, Employees as E.
@@ -55,13 +55,11 @@ FROM Department_Emp DE
 JOIN Employees E ON DE.emp_no = E.emp_no
 JOIN Departments D ON DE.dept_no = D.dept_no;
 
-
 List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
 --% is a wildcard that represents any number of characters.
 SELECT first_name, last_name, sex
 FROM Employees
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
-
 
 List each employee in the Sales department, including their employee number, last name, and first name.
 --aliases: Employees as E, Department_Emp as DE
@@ -70,7 +68,6 @@ FROM Employees E
 JOIN Department_Emp DE ON E.emp_no = DE.emp_no
 WHERE DE.dept_no = 'd007';
 
-
 List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
 --aliases :Employees as E, Department_Emp as DE, Departments as D.
 SELECT E.emp_no, E.last_name, E.first_name, D.dept_name
@@ -78,7 +75,6 @@ FROM Employees E
 JOIN Department_Emp DE ON E.emp_no = DE.emp_no
 JOIN Departments D ON DE.dept_no = D.dept_no
 WHERE D.dept_no IN ('d007', 'd005');
-
 
 List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
 SELECT last_name, COUNT(*) as name_count
