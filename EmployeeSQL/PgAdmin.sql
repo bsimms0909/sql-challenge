1074b7b1d06411030bbc1dfdc8cb0e7d9277ff8d
Nelson Ngwenya <bsimms0909@yahoo.com>
2023-11-07 18:11:00 -0600

    committing module 9

--- EmployeeSQL/PgAdmin.sql
+++ EmployeeSQL/PgAdmin.sql
@@ -43,25 +43,20 @@ FROM Employees
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
 
 --List each employee in the Sales department, including their employee number, last name, and first name.
---aliases: Employees as E, Department_Emp as DE
+-- Aliases: Employees as E, Department_Emp as DE
 SELECT E.emp_no, E.last_name, E.first_name
 FROM Employees E
 JOIN Department_Emp DE ON E.emp_no = DE.emp_no
-WHERE DE.dept_no = 'd007';
+WHERE DE.dept_no IN ('d007'); 
+
 
 --List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
 --aliases :Employees as E, Department_Emp as DE, Departments as D.
 SELECT E.emp_no, E.last_name, E.first_name, D.dept_name
-FROM Employees AS E
-JOIN Departments AS D ON E.dept_id = D.dept_id
+FROM Employees E
+INNER JOIN Department_Emp DE ON E.emp_no = DE.emp_no
+INNER JOIN Departments D ON DE.dept_no = D.dept_no
 WHERE D.dept_name IN ('Sales', 'Development');
-
---List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
-SELECT E.emp_no, E.last_name, E.first_name, D.dept_name
-FROM Employees AS E
-JOIN Departments AS D ON E.dept_id = D.dept_id
-WHERE D.dept_name IN ('Sales', 'Development');
-
 
 --List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
 SELECT last_name, COUNT(*) as name_count
@@ -70,4 +65,3 @@ GROUP BY last_name
 ORDER BY name_count DESC, last_name;
 
 
-----need to look at lines 52-63
